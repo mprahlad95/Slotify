b2e7fcc9c2af34b73227a22d46d5a5715cae6134
Prahlad Misra <mprahlad95@gmail.com>
2019-01-08 21:45:06 -0500

    login done - sessions final

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -9,6 +9,22 @@
 			$this->errorArray = array();
 		}
 
+		public function login($un, $pw) {
+
+			$pw = md5($pw);
+
+			$query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
+
+			if(mysqli_num_rows($query) == 1) {
+				return true;
+			}
+			else {
+				array_push($this->errorArray, Constants::$loginFailed);
+				return false;
+			}
+			
+		}
+
 		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
 			$this->validateUsername($un);
 			$this->validateFirstName($fn);
@@ -49,8 +65,12 @@
 				return;
 			}
 
-			//TODO: check if username exists
+			$checkUsernameQuery = mysqli_query($this->con, "SELECT username FROM users WHERE username='$un'");
 
+			if(mysqli_num_rows($checkUsernameQuery) != 0) {
+				array_push($this->errorArray, Constants::$usernameTaken);
+				return;
+			}
 		}
 
 		private function validateFirstName($fn) {
@@ -78,8 +98,12 @@
 				return;
 			}
 
-			//TODO: Check that username hasn't already been used
+			$checkEmailQuery = mysqli_query($this->con, "SELECT email FROM users WHERE email='$em'");
 
+			if(mysqli_num_rows($checkEmailQuery) != 0) {
+				array_push($this->errorArray, Constants::$emailTaken);
+				return;
+			}
 		}
 
 		private function validatePasswords($pw, $pw2) {
